17b6a3dc2d1afec6201cd65ca5194ca017787538
Noufel Gouirhate <ngouirha@amazon.com>
2021-09-19 16:25:50 +0200

    change to arbiscan

--- src/widgets/WalletModal/AccountModal.tsx
+++ src/widgets/WalletModal/AccountModal.tsx
@@ -23,8 +23,8 @@ const AccountModal: React.FC<Props> = ({ account, logout, onDismiss = () => null
       {account}
     </Text>
     <Flex mb="32px">
-      <LinkExternal small href={`https://bscscan.com/address/${account}`} mr="16px">
-        View on BscScan
+      <LinkExternal small href={`https://arbiscan.io/address${account}`} mr="16px">
+        View on Arbiscan
       </LinkExternal>
       <CopyToClipboard toCopy={account}>Copy Address</CopyToClipboard>
     </Flex>
